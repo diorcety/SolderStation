fadbe0c697395b4b223ec6d61fea63b18533de26
Yann Diorcet <diorcet.yann@gmail.com>
2015-05-06 19:03:27 +0200

    Fix MO prototype pinout

--- SolderStation/config_mo.h
+++ SolderStation/config_mo.h
@@ -8,7 +8,7 @@
 
 // Iron pins
 #define PIN_PWM_OUT 3
-#define PIN_PROBE_IN A0
+#define PIN_PROBE_IN A7
 
 // Controls
 #define ROTATING_UP_DOWN
@@ -21,7 +21,7 @@
 #define BUTTON_DOWN_INVERTED 1
 
 #define BUTTON_STANDBY
-#define BUTTON_STANDBY_PIN A2
+#define BUTTON_STANDBY_PIN A5
 #define BUTTON_STANDBY_PULL 1
 #define BUTTON_STANDBY_INVERTED 1
 
@@ -31,10 +31,10 @@
 #define SEG7_B_PIN 7
 #define SEG7_C_PIN 8
 #define SEG7_D_PIN 5
-#define SEG7_E_PIN A4
+#define SEG7_E_PIN A3
 #define SEG7_F_PIN 2
 #define SEG7_G_PIN 4
-#define SEG7_DP_PIN A5
+#define SEG7_DP_PIN A2
 #define SEG7_SEL0_PIN 11
 #define SEG7_SEL1_PIN 10
 #define SEG7_SEL2_PIN 9
@@ -42,7 +42,7 @@
 
 // LED Module
 #define LED_MODULE
-#define LED_HEAT A3
+#define LED_HEAT A4
 #define LED_HEAT_INVERTED 0
 
 // Serial Module
